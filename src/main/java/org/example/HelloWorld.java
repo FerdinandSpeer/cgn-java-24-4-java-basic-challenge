4070209ad9be9efc06265d23921687692482cf99
ferdinandspeer <ferdinandspeer@gmail.com>
2024-08-16 20:41:56 +0200

    first commit

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java is cool!");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        int difference = a - b; // Subtraktion
+        int product = a * b; // Multiplikation
+        int quotient = a / b; // Division
+        System.out.println(quotient);
+        System.out.println(product);
+        System.out.println(difference);
+        System.out.println(sum);
+
+    }
+}
